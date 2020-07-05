f25971ef90dd7081c2db5cd91febe8113b748d30
gohabs19 <57967821+gohabs19@users.noreply.github.com>
2020-07-05 15:19:01 -0400

    Update spacialAwareness.js
    Highlights, buttons, etc.

--- spatialAwareness/spacialAwareness.js
+++ spatialAwareness/spacialAwareness.js
@@ -3,10 +3,12 @@ let images=['1.jpg'];
 let choices=[['a', 'b', 'c', 'd']];
 let answers=['c'];//Correct answers to each question
 let choiceElements=[0,0,0,0];//Prefilled with 4 slots. We know each question has 4 choices.
-let button=[0,0,0,0];//Prefilled with 4 slots. We know each question has 4 choices, meaning that there are 4 buttons to be pressed.
+let answerButton=[0,0,0,0];//Prefilled with 4 slots. We know each question has 4 choices, meaning that there are 4 buttons to be pressed.
+let answerCorrectness=[0];//Keeps track of which answers are correct and incorrect in order. 0 indicates an incorrect answer, and 1 indicates a correct answer.
+let choiceMade=[0];//Keeps track of whether a choice has been made for each question. Once a choice has been made, a choice can no longer be made.
 
 function genQuestion(){
-    let i = 0;
+    let i = 0;//Used as a counter to create the options for the questions as buttons (e.g. button 'c').
     
     randomQuestionIndex = Math.floor(Math.random() * questions.length);
     
@@ -19,21 +21,30 @@ function genQuestion(){
     while ( i < choices[randomQuestionIndex].length ){
         choiceElements[i] = document.createElement('button');//Create button
         choiceElements[i].innerHTML = choices[randomQuestionIndex][i]; //Randomly selected question.
-        button[i]=document.getElementById('answers' + i).appendChild(choiceElements[i]);//Append button to answers elements in HTML
-        button[i].id=String.fromCharCode(i+97);//Create character for ascii value
-        button[i].onclick = function() {correctOrIncorrect(answers[randomQuestionIndex], this.id)};//On click, we compare answer to ID to see whether the answer was correct or incorrect
+        answerButton[i]=document.getElementById('answers' + i).appendChild(choiceElements[i]);//Append button to answers elements in HTML
+        answerButton[i].id=String.fromCharCode(i+97);//Create character for ascii value
+        answerButton[i].onclick = function() {
+            correctOrIncorrect(randomQuestionIndex, answers[randomQuestionIndex], this.id);//On click, we compare answer to ID to see whether the answer was correct or incorrect
+            console.log(answerCorrectness[randomQuestionIndex]);
+            if (answerCorrectness[randomQuestionIndex]===1) {//We display a green colour if correct answer chosen, red if incorrect chosen.
+                document.getElementById(this.id).style.background='#00FF00';//Green
+            }
+            else {
+                document.getElementById(this.id).style.background='#FF0000';//Red
+            }
+        }
         i++;
     }
 }
 
-function correctOrIncorrect(correctAnswer, id){//Checks if the button that has been pressed is the correct or incorrect choice
-    //console.log(correctAnswer, ' ', id);
-    
+function correctOrIncorrect(randomQuestionIndex, correctAnswer, id){//Checks if the button that has been pressed is the correct or incorrect choice
     if (correctAnswer===id) {//Show that this is the correct answer
         console.log("Correct");
+        answerCorrectness[randomQuestionIndex]=1;
     }
     else {//Show that this is the incorrect answer
-        console.log("Wrong")
+        console.log("Wrong");
+        answerCorrectness[randomQuestionIndex]=0;
     }
 }
 
